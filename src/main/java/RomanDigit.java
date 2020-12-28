90ebd1b2cbc5af79f25f37fed6359d035ee080ac
christopher <christopher.zelezny@mailbox.org>
2020-12-28 12:44:15 +0100

    refactor private enum to upper level

---
+++ src/main/java/RomanDigit.java
@@ -0,0 +1,25 @@
+enum RomanDigit {
+    M(1000),
+    CM(900),
+    D(500),
+    CD(400),
+    C(100),
+    XC(90),
+    L(50),
+    XL(40),
+    X(10),
+    IX(9),
+    V(5),
+    IV(4),
+    I(1);
+
+    private int value;
+
+    RomanDigit(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
